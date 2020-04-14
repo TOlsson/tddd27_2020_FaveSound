5aae84e52fb0a0d552d3f4b487179f9fbc76e2c8
Tobias Olsson <tobbe_col@hotmail.com>
2020-04-14 16:59:44 +0200

    Setup foundation for the server. Added dependencies, created connection to the database and initial testing of functionality.

---
+++ server/src/app.js
@@ -0,0 +1,45 @@
+// import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express(); // create your express app
+const secrets = require('../../secrets') // Secret information like db connection password
+// make app use dependencies
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+
+const mongo = require('mongodb')
+const MongoClient = mongo.MongoClient
+const uri = secrets.MongoDbConnectionString
+var client;
+
+var mongoClient = new MongoClient(uri, {
+    reconnectTries: Number.MAX_VALUE,
+    autoReconnect: true,
+    useNewUrlParser: true
+})
+
+mongoClient.connect((err, db) => {
+    if (err != null) {
+        console.log(err)
+        return
+    }
+    client = db;
+})
+
+app.get('/test', (req, res) => {
+    const collection = client.db("test").collection("test")
+
+    collection.find().toArray((err, result) => {
+        if (err) {
+            console.log(err);
+            res.send([]);
+            return;
+        }
+        res.send(result);
+    });
+});
+
+app.listen(process.env.PORT || 8081); // client is already running on 8080
