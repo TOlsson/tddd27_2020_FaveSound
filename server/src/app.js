40e3040acbe6a326212b0e8d2e6e5c09e1c9892e
Tobias Olsson <tobol101@student.liu.se>
2020-04-21 15:23:57 +0200

    Started on authentication for Google to collect access token from Front-End. Included foundations for Spotify authentication.

--- server/src/app.js
+++ server/src/app.js
@@ -4,11 +4,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const app = express(); // create your express app
+const SpotifyWebApi = require('spotify-web-api-node')
+const GoogleApi = require('googleapis')
 // make app use dependencies
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cors());
 
+// MongoDb initilization
 const mongo = require('mongodb')
 const MongoClient = mongo.MongoClient
 const uri = process.env.MongoDbConnectionString
@@ -27,6 +30,47 @@ mongoClient.connect((err, db) => {
     }
     client = db;
 })
+
+// Spotify Web Api initilization
+const spotifyClientId = `${process.env.SPOTIFY_CLIENT_ID}`
+const spotifyClientSecret = `${process.env.SPOTIFY_CLIENT_SECRET}`
+
+/* const spotifyApi = new SpotifyWebApi({
+    clientId: spotifyClientId,
+    clientSecret: spotifyClientSecret,
+    redirectUri: 'https://favesound.herokuapp.com/auth/spotify/callback'
+}) */
+
+const spotifyAuthOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+        'Authorization': 'Basic' + (new Buffer(spotifyClientId + ':' + spotifyClientSecret).toString('base64'))
+    },
+    form: {
+        grant_type: 'client_credentials'
+    },
+    json: true
+}
+
+app.get('/auth/spotify/callback', (req, res) => {
+
+})
+
+// Google Api initialization
+const googleClientId = `${process.env.GOOGLE_CLIENT_ID}`
+const googleClientSecret = `${process.env.GOOGLE_CLIENT_SECRET}`
+var googleToken;
+
+app.get('/auth/google/callback', (req, res) => {
+    
+})
+
+// Save access token from Google Authentication
+app.post('/googleLogin', (req, res) => {
+    googleToken = req.body.googleToken
+    res.send(200)
+})
+
 
 app.get('/test', (req, res) => {
     const collection = client.db("test").collection("test")
