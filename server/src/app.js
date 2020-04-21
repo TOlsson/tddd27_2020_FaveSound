cf385a3635f60afc7fe145326bb3655e3cbd98ae
Tobias Olsson <tobol101@student.liu.se>
2020-04-21 10:43:21 +0200

    Adde Google API and Spotify Web API libraries to the project. Some cleanup in the server code.

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const app = express(); // create your express app
-//const secrets = require('../../secrets') // Secret information like db connection password
 // make app use dependencies
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -13,8 +12,6 @@ app.use(cors());
 const mongo = require('mongodb')
 const MongoClient = mongo.MongoClient
 const uri = process.env.MongoDbConnectionString
-//const uri = 'mongodb+srv://default-user:'+process.env.mongoDB_secret+'@cluster0-6wvq8.gcp.mongodb.net/test?retryWrites=true&w=majority'
-console.log(process.env)
 var client;
 
 var mongoClient = new MongoClient(uri, {
