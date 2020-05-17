201377e2e45817d31b18e1dd07eb3010e2cff926
Tobias Olsson <tobol101@student.liu.se>
2020-05-17 17:03:56 +0200

    Forgot to change server url before commiting, changed back to heroku url.

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export default() => {
   return axios.create({
-    // baseURL: `https://favesound.herokuapp.com/` // the url of our heroku server
-    baseURL: 'http://localhost:5000/'
+    baseURL: `https://favesound.herokuapp.com/` // the url of our heroku server
+    // baseURL: 'http://localhost:5000/'
   })
 }
